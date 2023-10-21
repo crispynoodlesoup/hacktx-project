ca4e3f51837ec55cfc8c6c12a69bcd7083d941e7
crispynoodlesoup <thepanman321@mgmail.com>
2023-10-21 16:59:57 -0500

    setup a demo home page

--- src/App.jsx
+++ src/App.jsx
@@ -7,35 +7,48 @@ function App() {
     <>
       <nav>
         <ul>
-          <li>Home</li>
+          <li className="selected-page">Home</li>
           <li>Message</li>
           <li>Profile</li>
         </ul>
       </nav>
       <main>
-        <div className="header">
+        <section className="forum-header">
           <h2>Welcome back Crispynoodlesoup!</h2>
           <div className="account-div">
             <p>crispnoodlesoup</p>
             <div className="pfp"></div>
           </div>
-        </div>
-        <div className="forum">
-          <section>
-            <div className="posts">
-              <div className="user-post">
-                <h3>How do I construc</h3>
-                <h5>From: bender9000</h5>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores nemo soluta non deserunt rem esse quia expedita deleniti ullam amet, repellat doloribus totam earum fuga consectetur cupiditate. Quibusdam aspernatur accusantium, nostrum minima quidem ipsa, officia eum sint culpa quisquam quas, et voluptatibus corrupti. Quaerat, adipisci reiciendis quis esse iure numquam!</p>
-              </div>
-              <div className="user-post">
-                <h3>How do I construc</h3>
-                <h5>From: bender9000</h5>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores nemo soluta non deserunt rem esse quia expedita deleniti ullam amet, repellat doloribus totam earum fuga consectetur cupiditate. Quibusdam aspernatur accusantium, nostrum minima quidem ipsa, officia eum sint culpa quisquam quas, et voluptatibus corrupti. Quaerat, adipisci reiciendis quis esse iure numquam!</p>
-              </div>
+        </section>
+        <section className="forum">
+          <div className="posts">
+            <div className="user-post">
+              <h3>How do I construc</h3>
+              <h5>From: bender9000</h5>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores nemo soluta non deserunt rem esse quia expedita deleniti ullam amet, repellat doloribus totam earum fuga consectetur cupiditate. Quibusdam aspernatur accusantium, nostrum minima quidem ipsa, officia eum sint culpa quisquam quas, et voluptatibus corrupti. Quaerat, adipisci reiciendis quis esse iure numquam!</p>
             </div>
-          </section>
-        </div>
+            <div className="user-post">
+              <h3>How do I construc</h3>
+              <h5>From: bender9000</h5>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores nemo soluta non deserunt rem esse quia expedita deleniti ullam amet, repellat doloribus totam earum fuga consectetur cupiditate. Quibusdam aspernatur accusantium, nostrum minima quidem ipsa, officia eum sint culpa quisquam quas, et voluptatibus corrupti. Quaerat, adipisci reiciendis quis esse iure numquam!</p>
+            </div>
+            <div className="user-post">
+              <h3>How do I construc</h3>
+              <h5>From: bender9000</h5>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores nemo soluta non deserunt rem esse quia expedita deleniti ullam amet, repellat doloribus totam earum fuga consectetur cupiditate. Quibusdam aspernatur accusantium, nostrum minima quidem ipsa, officia eum sint culpa quisquam quas, et voluptatibus corrupti. Quaerat, adipisci reiciendis quis esse iure numquam!</p>
+            </div>
+            <div className="user-post">
+              <h3>How do I construc</h3>
+              <h5>From: bender9000</h5>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores nemo soluta non deserunt rem esse quia expedita deleniti ullam amet, repellat doloribus totam earum fuga consectetur cupiditate. Quibusdam aspernatur accusantium, nostrum minima quidem ipsa, officia eum sint culpa quisquam quas, et voluptatibus corrupti. Quaerat, adipisci reiciendis quis esse iure numquam!</p>
+            </div>
+            <div className="user-post">
+              <h3>How do I construc</h3>
+              <h5>From: bender9000</h5>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores nemo soluta non deserunt rem esse quia expedita deleniti ullam amet, repellat doloribus totam earum fuga consectetur cupiditate. Quibusdam aspernatur accusantium, nostrum minima quidem ipsa, officia eum sint culpa quisquam quas, et voluptatibus corrupti. Quaerat, adipisci reiciendis quis esse iure numquam!</p>
+            </div>
+          </div>
+        </section>
       </main>
     </>
   )

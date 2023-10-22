7657f26ff7b6b24d1d47bd568e8a0539fc38837e
crispynoodlesoup <thepanman321@mgmail.com>
2023-10-21 21:10:56 -0500

    display messages data onto page

--- src/Messages.jsx
+++ src/Messages.jsx
@@ -1,4 +1,41 @@
 import { Link } from "react-router-dom";
+const contactData = [
+  {
+    name: "john",
+    lastMessage: "boy what",
+  },
+  {
+    name: "keb",
+    lastMessage: "bom what",
+  },
+  {
+    name: "pee",
+    lastMessage: "boi what",
+  },
+];
+const messageData = [
+  {
+    id: "1",
+    content: "hey what the heck",
+    sender: "what",
+    recipient: "john",
+    date: "19-2-2009",
+  },
+  {
+    id: "2",
+    content: "hey whaattttt",
+    sender: "john",
+    recipient: "what",
+    date: "19-2-2009",
+  },
+  {
+    id: "3",
+    content: "boy what",
+    sender: "john",
+    recipient: "what",
+    date: "19-2-2009",
+  },
+];
 
 function Messages() {
   return (
@@ -16,7 +53,35 @@ function Messages() {
           </Link>
         </ul>
       </nav>
-      <main></main>
+      <main>
+        <div>
+          <div className="contacts">
+            <h3>Contacts</h3>
+            {contactData.map((contact) => {
+              return (
+                <div key={contact.name} className="user-contact">
+                  <p>
+                    <strong>{contact.name}</strong>
+                  </p>
+                  <p>
+                    <em>{contact.lastMessage}</em>
+                  </p>
+                </div>
+              );
+            })}
+          </div>
+          <div className="direct-messaging">
+            {messageData.map((message) => {
+              return (
+                <div key={message.id} className="message">
+                  <p>{`${message.sender} - ${message.date}`}</p>
+                  <p>{message.content}</p>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </main>
     </>
   );
 }
